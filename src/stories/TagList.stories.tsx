27b25d5d2e7fb94a32b8283c6536b5e39884989f
By_juun <neostgeart@gmail.com>
2024-03-17 23:20:14 +0900

    feat: TagList Component

---
+++ src/stories/TagList.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import TagList from "../components/TagList";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "Lists/TagList",
+  component: TagList,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    tagList: {
+      defaultValue: ["1~2명", "3~5명", "6~8명", "9~10명"],
+      description: "태그 리스트",
+      control: "array",
+    },
+  },
+} satisfies Meta<typeof TagList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    tagList: ["1~2명", "3~5명", "6~8명", "9~10명"],
+    onTagClick: fn,
+  },
+};
